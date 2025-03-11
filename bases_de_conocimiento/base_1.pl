fd8428bee4423460eff77ac2a91103adf26c568a
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-03-11 17:16:19 -0500

    Se agrega una base de conocimiento sencilla

---
+++ bases_de_conocimiento/base_1.pl
@@ -0,0 +1,20 @@
+% Hechos - Proposiciones simples
+
+% En un archivo prolog escribimos hechos y reglas que son lo que conocemos como premisas de un argumento. 
+
+% Escribamos hechos, para que Prolog tenga contexto de la situación que queremos plantear. 
+% Supongamos que queremos consultar cuando algo es mortal o no. Entonces escribamos hechos definiendo sujetos y sus propiedades. 
+
+
+% Hechos
+persona(maría).
+persona(antonio).
+objeto(miImpresora).
+objeto(miCelular).
+animal(miVaca).
+
+% Reglas
+mortal(X) :- persona(X).
+mortal(X) :- animal(X).
+no_mortal(X) :- objeto(X). 
+
